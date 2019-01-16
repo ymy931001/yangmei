38960f98d6d790f262f64c847fcc7ddb50fcefea
yumaoyang330 <37444950+yumaoyang330@users.noreply.github.com>
2019-01-16 14:10:33 +0800

    1.16

--- src/Consumer/Consumer.js
+++ src/Consumer/Consumer.js
@@ -451,9 +451,9 @@ export default class Consumer extends Component {
                 <div className="section4">
                   <div><img src={require('./brand1.png')} alt="logo" /></div>
                   <p className="css12594a6bbd1d0b6">扫描下方二维码了解更详细的信息</p>
-                  <div><img src={require('./brand3.png')} alt="logo" style={{ marginTop: '.3rem', width: '2.5rem', height: 'auto' }} /></div>
-                  <div><img src={require('./brand2.png')} alt="logo" style={{ marginTop: '.25rem', width: '3rem', height: 'auto' }} /></div>
-                  <div><img src={require('./brand4.png')} alt="logo" style={{ marginTop: '.25rem', width: '2rem', height: 'auto' }} /></div>
+                  <div><img src={require('./brand3.png')} alt="logo" style={{ marginTop: '.25rem', width: '2.5rem', height: 'auto' }} /></div>
+                  <div><img src={require('./brand2.png')} alt="logo" style={{ marginTop: '.2rem', width: '3rem', height: 'auto' }} /></div>
+                  <div><img src={require('./brand4.png')} alt="logo" style={{ marginTop: '.2rem', width: '2rem', height: 'auto' }} /></div>
 
                   <p className="Consumer-bottombar css12594a6bbd1d0b6">技术保障：杭州移动 浙江大学</p>
                   <p className="Consumer-bottombar css12594a6bbd1d0b6">实施单位：钛比科技  杭州径山茶叶有限公司（客服热线：13666605688）</p>
