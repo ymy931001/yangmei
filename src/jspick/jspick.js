7a59c8e14ef48d5caf4894d35f6d540b098e68c9
yumaoyang330 <37444950+yumaoyang330@users.noreply.github.com>
2019-04-01 16:56:48 +0800

    4.1

--- src/jspick/jspick.js
+++ src/jspick/jspick.js
@@ -19,17 +19,17 @@ export default class Consumer extends Component {
       <div className="Cook-body">   
           <div className="address css12594a6bbd1d0b6">实景采摘地点</div>
           {/* <div className="address css12594a6bbd1d0b6"></div> */}
-          <div className="clearfix pack-main">
-            <div className="Cook-text">
+          {/* <div className="clearfix pack-main">
+            <div className="Cook-text"> */}
               {/* <img src={require('./map.png')} alt="pic" />
               <div className="cook-top css12594a6bbd1d0b6">
               {this.state.gardenID}</div>
               <div className="Cook-introduction css12594a6bbd1d0b6">
               您手中的茶叶产自于杭州径山茶叶有限公司的茶叶产自于径山茶核心产区——径山寺山顶西侧的唐代千年历史茶地。径山万寿禅寺雄踞江南禅院“五山十刹”之首，号称“东南第一禅院”。径山灵山毓秀，渌水含英，蕙若暨芝兰并茂，芳苓与瑞树同生。
               </div> */}
-            </div>
-          </div>
-          <div className="Cook-img">
+            {/* </div>
+          </div> */}
+          <div className="Cook-img" style={{marginTop:'1rem'}}>
             <img className="Cook-imgimg" src={this.state.img}   alt="pic" />
             <div className="Cook-imgtext css12594a6bbd1d0b6">{this.state.pickTime}<span>实景拍摄于茶园</span></div>
           </div>
