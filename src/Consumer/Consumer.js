c02e7e6be7cb8c9d3cac04eecc19b965497c0af0
yumaoyang330 <37444950+yumaoyang330@users.noreply.github.com>
2019-04-04 15:01:46 +0800

    4.4

--- src/Consumer/Consumer.js
+++ src/Consumer/Consumer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Flex, Button, InputItem, Toast, WhiteSpacem, Carousel, WhiteSpace, WingBlank } from 'antd-mobile';
 import { getInfoForConumer, fake } from "../axios";
 import './Consumer.css';
+import './taishun.css';
 import * as echarts from 'echarts';
 import { generateGetCodeUrl } from '../util';
 import URI from 'urijs';
@@ -42,7 +43,7 @@ export default class Consumer extends Component {
           let { letcode } = query;
           let localcount = localStorage.getItem('count');
           let _this = this;
-          
+
           let data = res.data;
           let garden = res.data.teaInfo.gardenInfo;
           let pick = res.data.teaInfo.pickInfo;
@@ -468,6 +469,204 @@ export default class Consumer extends Component {
               </div>
             });
           }
+
+          if (teaType === "泰顺") {
+            document.title = "杭州径山茶叶有限公司茶园—让数据说话";
+            this.setState({
+              content: <div style={{ width: '100%' }} id="taishun_body">
+                <div className="banner">
+                  <img src={require('./banner2.png')} />
+                </div>
+                <div className="section">
+                  <div className="sub-title css12594a6bbd1d0b6">径山茶</div>
+                  <div className="sub-title1 css12594a6bbd1d0b6">全球唯一追溯ID: <span>{data.teaInfo.teaID}</span></div>
+                  <WingBlank>
+                    <Carousel className="space-carousel"
+                      frameOverflow="visible"
+                      cellSpacing={10}
+                      slideWidth={0.5}
+                      autoplay
+                      dots={false}
+                      infinite
+                      afterChange={index => this.setState({ slideIndex: index })}
+                    >
+                      {this.state.data.map((val, index) => (
+                        <span>
+                          <img
+                            src={require(`./img1/${val}.png`)}
+                            alt=""
+                            style={{ width: '47%', verticalAlign: 'top' }}
+                            onLoad={() => {
+                              // fire window resize event to change height
+                              window.dispatchEvent(new Event('resize'));
+                              this.setState({ imgHeight: 'auto' });
+                            }}
+                          />
+                          <img
+                            src={require(`./img2/${val}.png`)}
+                            alt=""
+                            style={{ width: '47%', verticalAlign: 'top', marginLeft: '6%' }}
+                            onLoad={() => {
+                              // fire window resize event to change height
+                              window.dispatchEvent(new Event('resize'));
+                              this.setState({ imgHeight: 'auto' });
+                            }}
+                          />
+                        </span>
+                      ))}
+                    </Carousel>
+                  </WingBlank>
+                  <div className="sub-content css12594a6bbd1d0b6">
+                    中国移动联合浙江大学以径山寺山顶西侧1公里处高品质茶叶基地，无污染有机茶园为试点；利用物联网及区块链技术，实现了径山茶种植、制作等环节的数据追溯，让数据替茶说话。
+                </div >
+                </div>
+                <div className="section1">
+                  <div className="sub-title5 css12594a6bbd1d0b6">您手中的茶叶来自于</div>
+                  <div className="comefrom">
+                    <div className="map">
+                      <img
+                        className="mapimg"
+                        src={require('./map2.jpg')}
+                        alt=""
+                        onLoad={() => {
+                          window.dispatchEvent(new Event('resize'));
+                          this.setState({ imgHeight: 'auto' });
+                        }}
+                      />
+                    </div>
+                    <div className="map-right">
+                      <div style={{ fontSize: '0.22rem', marginBottom: '0.1rem', textAlign: 'center' }} className="css12594a6bbd1d0b6">{garden.name}</div>
+                      <div style={{ textAlign: 'center', fontSize: '0.19rem' }} className="css12594a6bbd1d0b6">东经：{garden.details.latitude} 北纬：{garden.details.longitude}</div>
+                      <div className="map-text css12594a6bbd1d0b6">
+                      香菇寮，位于彭溪镇海拔四百多米的山岙之中，荟蔚苍萃，停云住霭，云烟起时犹如仙境，是孕育香菇寮白毫的一方净土。据多次移植试验证实，这里的地理环境对香菇寮白毫的种植具有不可复制性，其他地方尚无法培育出纯正味道的香菇寮白毫，因此种植规模非常有限，产量稀少，在市场上“一茶难求”，极为珍贵。
+                      </div>
+                    </div>
+                    <div style={{ fontSize: "0.19rem", float: 'left', width: '96%', lineHeight: '.265rem', marginTop: '.05rem' }}>采彼谷雨之芽，储诸佳缶；汲此双溪之泉，煮以美甑。猗欤香芬之清醇，正堪礼佛；休哉汤泽之澄碧，雅可作贡。余以自饮，于氤氲中明见禅心；兼以饮客，自机锋里证得空境。径山由此诞生径山的禅和茶，辨空色为非相，离相即佛；融禅茶于一味，知味了性。以是宴具三事，寓修行于嘉会；茶入六品，驰令誉于上邦。鸿渐南来，烹石泉于苕畔；茗艺东播，弘茶道于扶桑。径山茶，始栽于唐，闻名于宋，至今有1200余年历史。苏轼为径山常客，欲安迟暮；陆游乃方外素友，思寄余闲。他如子由胜览，蔡襄雅谈，王阳明隐迹莽野，徐青藤纵情竹弦。皆曾云鹤为侣，栖迟于林间月下；山猿作邻，啸咏于崖涘花前。秋弄丹桂，倚赤松之修茂；春访茑萝，濯碧流之潺湲。以是诗章海积，铭辞星繁，行述俱美，人文相缘。视此禅那之国土，不异翰场；观我浙杭之风标，几于此山。径山由此以“崇尚自然，讲究真色、真香、真味”独树一帜，更以“名山、名寺、名人、名茶”著称于世，铸就千古流传的《茶经》。</div>
+                  </div>
+                  <div className="pick1">
+                    <img src={require('./caizhai.png')} style={{ width: '100%', height: '2.07rem', verticalAlign: 'top', float: 'left', }} />
+                  </div>
+                  <div className="sub-title3 css12594a6bbd1d0b6" >手工采摘品质保证</div>
+                  <div className="sub-content css12594a6bbd1d0b6"
+                  >径山茶以采摘细嫩著称，春茶以一叶一芽为采摘标准。清明前后采摘的茶叶为品质最佳，采茶姑娘们踏着清晨露水，用一双熟悉的巧手摘下饱满鲜嫩的茶叶尖。</div>
+                  <div style={{ textAlign: 'center' }}>
+                    <a href="/consumer/jspick" className="Consumer-more css12594a6bbd1d0b6">实时图片</a>
+                  </div>
+                </div>
+                <div className="section2">
+                  <WingBlank>
+                    <Carousel className="space-carousel"
+                      frameOverflow="visible"
+                      cellSpacing={10}
+                      slideWidth={0.5}
+                      autoplay
+                      dots={false}
+                      infinite
+                      afterChange={index => this.setState({ slideIndex: index })}
+                    >
+                      {this.state.data.map((val, index) => (
+                        <span>
+                          <img
+                            src={require(`./img3/${val}.png`)}
+                            alt=""
+                            style={{ width: '47%', verticalAlign: 'top' }}
+                            onLoad={() => {
+                              // fire window resize event to change height
+                              window.dispatchEvent(new Event('resize'));
+                              this.setState({ imgHeight: 'auto' });
+                            }}
+                          />
+                          <img
+                            src={require(`./img4/${val}.png`)}
+                            alt=""
+                            style={{ width: '47%', verticalAlign: 'top', marginLeft: '6%' }}
+                            onLoad={() => {
+                              // fire window resize event to change height
+                              window.dispatchEvent(new Event('resize'));
+                              this.setState({ imgHeight: 'auto' });
+                            }}
+                          />
+                        </span>
+                      ))}
+                    </Carousel>
+                  </WingBlank>
+                  <div className="sub-title3 css12594a6bbd1d0b6">传承悠久的炒茶技艺</div>
+                  <div className="sub-content css12594a6bbd1d0b6">由30年以上炒茶经验的炒茶师，经鲜叶摊放—杀青（手工）—理条（手工）—揉捻（手工）—干燥五道工序制作而成。</div>
+                  <div style={{ textAlign: 'center' }}>
+                    <a href="/consumer/jscook" className="Consumer-more css12594a6bbd1d0b6">实时图片</a>
+                  </div>
+                  <div className="pick2">
+                    <img src={require('./packing2.png')} style={{ width: '100%', height: '2.73rem', float: 'left', }} />
+
+                  </div>
+                  <div className="sub-title3 css12594a6bbd1d0b6">用科技包裹风味与历史</div>
+                  <div className="sub-content css12594a6bbd1d0b6">使用塑料封独立包装最大程度保持白毫茶的正宗风味，用古朴与科技相融的包装带您体验千年古茶与现代科技的完美融合。</div>
+                </div>
+                <div className="section3">
+                  <div className="sub-title2 css12594a6bbd1d0b6">让数据为白毫茶发声</div>
+                  <Flex justify="center" direction="column" >
+                    <Flex justify="center">
+                      <div className="Consumer-dataimg css12594a6bbd1d0b6"><span className="pm-data">{pmtext}</span></div>
+                      <div className="Consumer-data mt3">
+                        <div className="data-title css12594a6bbd1d0b6" >PM2.5：<span style={{ fontSize: '.22rem' }}>{pm25}</span><span style={{ fontSize: '.22rem' }}>𝜇𝑔/𝑚<sup>3</sup></span>
+                        </div>
+                        <WhiteSpace />
+                        <div className="Consumer-datadetail css12594a6bbd1d0b6">
+                          PM2.5是衡量茶园空气质量和生态水平的重要参数，良好的空气质量将使茶叶的氨基酸和水浸出物含量维持在适宜水平，保证茶叶口感和滋味的醇正。
+                      </div>
+                      </div>
+                    </Flex>
+                    <Flex justify="center">
+                      <div className="Consumer-dataimg"><span className="light-data">{lighttext}</span></div>
+                      <div className="Consumer-data mt2">
+                        <div className="data-title css12594a6bbd1d0b6">光照：<span>{light}</span><span>𝑙𝑥</span></div>
+                        <WhiteSpace />
+                        <div className="Consumer-datadetail css12594a6bbd1d0b6">
+                          适宜、充足的光照可以促进光合产物的形成，使茶树生长更加茁壮，同时也会影响茶叶中咖啡碱的含量，令茶叶品质更佳。
+                      </div>
+                      </div>
+                    </Flex>
+                    <Flex justify="center">
+                      <div className="Consumer-dataimg" id="tem-chart">
+                      </div>
+                      <div className="Consumer-data mt4">
+                        <div className="data-position css12594a6bbd1d0b6">数据点/3天</div>
+                        <div className="data-title css12594a6bbd1d0b6">土壤温度</div>
+                        <WhiteSpace />
+                        <div className="Consumer-datadetail css12594a6bbd1d0b6">
+                          适宜的土壤温度能够促进土壤的呼吸作用，使茶树获得更多营养、健康成长。
+                      </div>
+                      </div>
+                    </Flex>
+                    <Flex justify="center">
+                      <div className="Consumer-dataimg" id="hum-chart">
+
+                      </div>
+                      <div className="Consumer-data mt1">
+                        <div className="data-position1 css12594a6bbd1d0b6">数据点/3天</div>
+                        <div className="data-title css12594a6bbd1d0b6">土壤湿度</div>
+                        <WhiteSpace />
+                        <div className="Consumer-datadetail css12594a6bbd1d0b6">
+                          土壤湿度是影响茶树生长速度的关键因素，适宜的土壤湿度可以将茶叶中的氨基酸含量维持在适宜水平，使茶叶口感更加鲜爽。
+                      </div>
+                      </div>
+                    </Flex>
+                  </Flex>
+                </div>
+                <div className="section4">
+                  <div><img src={require('./brand1.png')} alt="logo" style={{ width: '3rem', height: 'auto' }} /></div>
+                  <div><img src={require('./brand3.png')} alt="logo" style={{ marginTop: '.25rem', width: '2.5rem', height: 'auto' }} /></div>
+                  <div><img src={require('./brand2.png')} alt="logo" style={{ marginTop: '.2rem', width: '3rem', height: 'auto' }} /></div>
+                  <div><img src={require('./brand4.png')} alt="logo" style={{ marginTop: '.2rem', width: '2rem', height: 'auto' }} /></div>
+                  <p className="Consumer-bottombar css12594a6bbd1d0b6">杭州径山茶叶有限公司</p>
+                  <p>客服热线：13666605688</p>
+                  <p className="Consumer-bottombar css12594a6bbd1d0b6">技术保障：杭州移动 浙江大学</p>
+                  <p className="Consumer-bottombar css12594a6bbd1d0b6">实施单位：钛比科技  </p>
+                </div>
+              </div>
+            });
+          }
+
           localStorage.setItem('cookIntroduction', cook.introduction);
           localStorage.setItem('cookTime', cook.cookTime);
           localStorage.setItem('cookName', cook.name);
