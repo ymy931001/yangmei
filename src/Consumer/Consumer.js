420fda47ad2320ccbc3c8ba7c647cc976280b874
yumaoyang330 <37444950+yumaoyang330@users.noreply.github.com>
2019-04-05 09:19:39 +0800

    4.5

--- src/Consumer/Consumer.js
+++ src/Consumer/Consumer.js
@@ -638,11 +638,7 @@ export default class Consumer extends Component {
                   <div style={{ fontSize: ".25rem", lineHeight: '.4rem' }}>包装时间：{this.state.packTime}</div>
                 </div>
                 <div className="section4">
-
-                  {/* <div><img src={require('./brand1.png')} alt="logo" style={{ width: '3rem', height: 'auto' }} /></div>
-                  <div><img src={require('./brand3.png')} alt="logo" style={{ marginTop: '.25rem', width: '2.5rem', height: 'auto' }} /></div>
-                  <div><img src={require('./brand2.png')} alt="logo" style={{ marginTop: '.2rem', width: '3rem', height: 'auto' }} /></div>
-                  <div><img src={require('./brand4.png')} alt="logo" style={{ marginTop: '.2rem', width: '2rem', height: 'auto' }} /></div> */}
+        
                   <div className="Consumer-bottombar css12594a6bbd1d0b6" style={{ fontSize: '.28rem', lineHeight: '.5rem' }}>
                     <img src={require('./botlogo.png')} alt="logo" style={{ width: ".3rem", height: '.3rem' }} />
                     泰顺县广和农业开发有限公司
