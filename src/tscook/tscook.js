b14e740fca0f013127e52ad4845d4ae665a3b29a
yumaoyang330 <37444950+yumaoyang330@users.noreply.github.com>
2019-04-08 14:43:46 +0800

    4.8

--- src/tscook/tscook.js
+++ src/tscook/tscook.js
@@ -18,21 +18,23 @@ export default class Consumer extends Component {
 
   render() {
     return (
-      <div className="tsCook-body">
+      <div id="tscook">
+        <div className="Cook-body">
           <div className="address css12594a6bbd1d0b6">实景炒制</div>
           <div className="clearfix">
             <div className="Cook-text">
               <img src={this.state.cookImg} alt="pic" />
               <div className="cook-top">
-              <span className="cook-author css12594a6bbd1d0b6">{this.state.cookName}</span>
-              <span className="cook-time css12594a6bbd1d0b6">{this.state.identification}</span></div>
+                <span className="cook-author css12594a6bbd1d0b6">{this.state.cookName}</span>
+                <span className="cook-time css12594a6bbd1d0b6">{this.state.identification}</span></div>
               <div className="Cook-introduction css12594a6bbd1d0b6">{this.state.introduction}</div>
             </div>
           </div>
           <div className="Cook-img">
-            <img className="Cook-imgimg" src={this.state.cookInfoImg}  alt="pic" />
+            <img className="Cook-imgimg" src={this.state.cookInfoImg} alt="pic" />
             <div className="Cook-imgtext css12594a6bbd1d0b6">{this.state.cookTime}<span>为您炒制出您手中的茶叶</span></div>
           </div>
+        </div>
       </div>
     )
   }
