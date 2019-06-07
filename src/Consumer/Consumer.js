5744131b3ab47f85816e802f07e69d2dbab60ed7
yumaoyang <yumaoyang@terabits.cn>
2019-06-07 14:37:25 +0800

    6.7

--- src/Consumer/Consumer.js
+++ src/Consumer/Consumer.js
@@ -504,7 +504,7 @@ export default class Consumer extends Component {
               </div>
             });
           }
-          if (teaType === "泰顺") {
+          if (teaType === "杜家杨梅") {
             document.title = "泰顺县广和农业开发有限公司—让数据说话";
             this.setState({
               content: <div style={{ width: '100%' }} id="taishun_body">
@@ -672,7 +672,7 @@ export default class Consumer extends Component {
               </div>
             });
           }
-          if (teaType === "杜家杨梅") {
+          if (teaType === "泰顺") {
             document.title = "杜家村杨梅—让数据说话";
             this.setState({
               content: <div style={{ width: '100%' }} id="taishun_body">
